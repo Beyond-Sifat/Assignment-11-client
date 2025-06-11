90d1a4f1c45161664ba36cd3aa32926cd527c531
student-webdev-ph <mdaltafuddin456@gmail.com>
2025-06-11 21:38:46 +0600

    details page functionality

--- src/Pages/FoodDetails.jsx
+++ src/Pages/FoodDetails.jsx
@@ -1,19 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useLoaderData } from 'react-router';
 import { AuthContext } from '../Context/AuthContext';
 
 const FoodDetails = () => {
     const details = useLoaderData()
     const { user } = useContext(AuthContext)
-    console.log(details.email, user)
+    const [loading, setLoading] = useState(false)
 
 
-
-
+    // if expiry
     const isExpired = (expiryDate) => {
         return new Date(expiryDate) < new Date()
     }
 
+    // if not expiry
     const daysLeft = (expiryDate) => {
         const now = new Date()
         const expiry = new Date(expiryDate);
@@ -21,9 +21,10 @@ const FoodDetails = () => {
         const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
         return diffDays
     }
+    const expired = isExpired(details.expiryDate);  // if expiry
+    const left = daysLeft(details.expiryDate)      // if not expiry
 
-    const expired = isExpired(details.expiryDate);
-    const left = daysLeft(details.expiryDate)
+    const canAddNote = user?.email === details.email
 
 
 
@@ -46,21 +47,25 @@ const FoodDetails = () => {
                         <p className='text-lg text-gray-600 mb-4'>Category: <span className='font-semibold'>{details.category}</span></p>
                         <p className='text-lg mb-2'>Quantity: <span className='font-semibold'>{details.quantity}</span></p>
                         <p className='text-lg mb-2'>Expiry Date: <span className='font-semibold'>{details.quantity}</span></p>
-
                         {
                             expired ? (
-                                <span className="inline-block bg-red-600 text-white px-3 py-1 rounded-full font-semibold mt-2">Expired</span>
+                                <span className="inline-block bg-red-600 text-white px-3 py-1 rounded-full font-semibold mt-2">Expired</span>  // if expiry
                             ) : (
-                                <p className="text-green-600 font-semibold mt-2">{left} day{left !== 1 ? 's' : ""} left until expiry</p>
+                                <p className="text-green-600 font-semibold mt-2">{left} day{left !== 1 ? 's' : ""} left until expiry</p>         // if not expiry
                             )
                         }
-
                         <h2 className="text-2xl font-semibold mt-6 mb-2">Description</h2>
                         <p className="text-gray-700 whitespace-pre-line">
                             {details.description || "No description provided."}
                         </p>
                     </div>
                 </div>
+            </div>
+            <div className='mt-10'>
+                <button
+                // type='submit'
+                disabled={!canAddNote}
+                className="btn btn-primary mt-3">{loading ? "Adding" : "Add Note"}</button>
             </div>
         </div>
     );
